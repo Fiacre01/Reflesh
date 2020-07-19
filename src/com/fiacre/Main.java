4577e548e85f6424012d6a7d3bf22e11ef36a3b5
Fiacre01 <indagiye@gmail.com>
2020-07-18 22:59:09 -0400

    first commit

---
+++ src/com/fiacre/Main.java
@@ -0,0 +1,29 @@
+package com.fiacre;
+
+import java.sql.SQLOutput;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name= "  Fiacre \"Indagiye\" Sibo   ";
+        int age= 25;
+        int money= 111_111_111;
+        System.out.println(age);
+        System.out.println(money);
+        Date now = new Date();
+        int randomInt= (int) (Math.random()*9)+1;
+        System.out.println("random integer = " +randomInt);
+        System.out.println(now);
+        String newName =  name.trim();
+        System.out.println(name+", "+ name.length());
+        System.out.println(newName +", "+ newName.length());
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        System.out.println(formatter.format(now));
+
+
+    }
+}
