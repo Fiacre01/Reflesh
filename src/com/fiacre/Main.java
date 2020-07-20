8c14b97b1c4cad5e1e09d212615e864d9a19bd5d
Fiacre01 <indagiye@gmail.com>
2020-07-20 01:37:16 -0400

    added more string modifications

--- src/com/fiacre/Main.java
+++ src/com/fiacre/Main.java
@@ -6,41 +6,59 @@ import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        String name= "  Fiacre Indagiye Sibo   ";
-        String firstName= "Fiacre";
-        String lastName= "Sibo";
-        Scanner sc = new Scanner(System.in);
-        System.out.println("please enter the middle name: ");
-        String middleName= sc.next();
-            if (middleName.startsWith("i") || middleName.contains("s")) {
-                System.out.println("please enter a different middle name");
-            }
-            else if (middleName.contains("d")){
-                System.out.println("Thank you");
-            }
-        System.out.println(firstName + " " + " " + middleName + " " + lastName);
-        int age= 25;
         int num1=10;
         int num2=20;
-        int money= 111_111_111;
+        String fullName="";
+        String firstName= "John";
+        String lastName= "Smith";
 
-        Date now = new Date();
+
         int randomInt= (int) (Math.random()*9)+1;
         System.out.println("random integer = " +randomInt);
-        System.out.println(now);
-        String newName =  name.trim();
-        System.out.println(name+", "+ name.length());
-        System.out.println(newName +", "+ newName.length());
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(formatter.format(now));
+
         Addition add= new Addition();
         int sum = add.addTwoNum(num1,num2);
-        System.out.println(sum);
+        System.out.println("The sum of both numbers is: " +sum);
+
+        strings(firstName,lastName,fullName);
+
+        dates();
+
+    }
+
+    public static void strings(String firstName, String lastName,String fullName){
+
+        //getting middle name
+        Scanner sc = new Scanner(System.in);
+        String middleName;
+        do {
+            System.out.println("please enter the middle name: ");
+            middleName = sc.next();
+        }
+        while (middleName.startsWith("i"));
+
+        //joining first,middle,last names
+        System.out.println("adding first, middle and last names together: ");
+        System.out.println(firstName+middleName+lastName);
+        fullName = String.join(" ",firstName,middleName,lastName);
+        System.out.println(fullName+ ", " + fullName.length());
+
+        // changing name to uppercase
+        System.out.println("making name uppercase: ");
+        String newName =  fullName.toUpperCase();
+        System.out.println(newName +", "+ newName.length());
+    }
+    public static void dates(){
+        Date now = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+
+        System.out.println("The time is:" +formatter.format(now));
 
 
     }
