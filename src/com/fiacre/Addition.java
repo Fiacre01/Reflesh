586b029c2ae4fbdb81db5b7c06be6c2041a6f894
Fiacre01 <indagiye@gmail.com>
2020-07-20 00:16:31 -0400

    added new addition class

---
+++ src/com/fiacre/Addition.java
@@ -0,0 +1,10 @@
+package com.fiacre;
+
+public class Addition {
+    int sum = 0;
+    public int addTwoNum(int a,int b){
+        sum = a+b;
+
+        return sum;
+    }
+}
--- src/com/fiacre/Main.java
+++ src/com/fiacre/Main.java
@@ -4,13 +4,29 @@ import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name= "  Fiacre \"Indagiye\" Sibo   ";
+
+        String name= "  Fiacre Indagiye Sibo   ";
+        String firstName= "Fiacre";
+        String lastName= "Sibo";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter the middle name: ");
+        String middleName= sc.next();
+            if (middleName.startsWith("i") || middleName.contains("s")) {
+                System.out.println("please enter a different middle name");
+            }
+            else if (middleName.contains("d")){
+                System.out.println("Thank you");
+            }
+        System.out.println(firstName + " " + " " + middleName + " " + lastName);
         int age= 25;
+        int num1=10;
+        int num2=20;
         int money= 111_111_111;
 
         Date now = new Date();
@@ -22,7 +38,9 @@ public class Main {
         System.out.println(newName +", "+ newName.length());
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
         System.out.println(formatter.format(now));
-
+        Addition add= new Addition();
+        int sum = add.addTwoNum(num1,num2);
+        System.out.println(sum);
 
 
     }
